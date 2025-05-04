7ce913fb8061deb7072737584fcf4bb8d2e7b30b
Osamikoyo <v.pylayev@mail.ru>
2025-05-04 05:54:01 +0500

    add config

--- go.mod
+++ go.mod
@@ -3,11 +3,13 @@ module github.com/Koyo-os/form-service
 go 1.24.2
 
 require (
+	github.com/caarlos0/env/v10 v10.0.0 // indirect
 	github.com/go-sql-driver/mysql v1.7.0 // indirect
 	github.com/golang/snappy v0.0.4 // indirect
 	github.com/google/uuid v1.6.0 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
 	github.com/klauspost/compress v1.16.7 // indirect
 	github.com/montanaflynn/stats v0.7.1 // indirect
 	github.com/xdg-go/pbkdf2 v1.0.0 // indirect
@@ -20,6 +22,7 @@ require (
 	golang.org/x/crypto v0.26.0 // indirect
 	golang.org/x/sync v0.13.0 // indirect
 	golang.org/x/text v0.24.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 	gorm.io/driver/mysql v1.5.7 // indirect
 	gorm.io/gorm v1.26.0 // indirect
 )
---
+++ pkg/config/config.go
@@ -0,0 +1,40 @@
+package config
+
+import (
+	"fmt"
+	"os"
+
+	"gopkg.in/yaml.v3"
+)
+
+type Config struct {
+	Reqs struct {
+		CreateRequestType string `yaml:"create_req_type"`
+		UpdateRequestType string `yaml:"update_req_type"`
+		DeleteRequestType string `yaml:"delete_req_type"`
+	} `yaml:"reqs"`
+	RabbitmqUrl     string `yaml:"rabbitmq_url"`
+	RequestExchange string `yaml:"req_exchange"`
+	OutputExcange   string `yaml:"out_exchange"`
+	Dsn             string `yaml:"dsn"`
+	QueueName       string `yaml:"queue_name"`
+}
+
+func Init(path string) (*Config, error) {
+	var cfg Config
+
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, fmt.Errorf("error open file: %v", err)
+	}
+
+	defer file.Close()
+
+	if err = yaml.NewDecoder(file).Decode(&cfg); err != nil {
+		return nil, fmt.Errorf("decode error: %v", err)
+	}
+
+	cfg.QueueName = "que"
+
+	return &cfg, nil
+}
