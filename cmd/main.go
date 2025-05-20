f94ec8d1072370c83620ee769bba5aa5aef2d648
Osamikoyo <v.pylayev@mail.ru>
2025-05-20 05:58:03 +0500

    update main.go

--- cmd/main.go
+++ cmd/main.go
@@ -1,4 +1,33 @@
 package main
 
+import (
+	"github.com/Koyo-os/form-service/pkg/config"
+	"github.com/Koyo-os/form-service/pkg/logger"
+	"go.uber.org/zap"
+)
+
 func main() {
+	logCfg := logger.Config{
+		LogFile:   "app.log",
+		LogLevel:  "debug",
+		AppName:   "form-service",
+		AddCaller: true,
+	}
+
+	if err := logger.Init(logCfg); err != nil {
+		panic(err)
+	}
+
+	defer logger.Sync()
+
+	logger := logger.Get()
+
+	cfg, err := config.Init(".env")
+	if err != nil {
+		logger.Error("error init config",
+			zap.String("path", ".env"),
+			zap.Error(err))
+		return
+	}
+
 }
