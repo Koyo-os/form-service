7dd0e1ec43d76543cf7c7b82e57ff3312e8571c1
Osamikoyo <v.pylayev@mail.ru>
2025-05-21 07:10:26 +0500

    update interfaces

--- internal/service/interfaces.go
+++ internal/service/interfaces.go
@@ -11,6 +11,7 @@ type (
 	Repository interface {
 		Create(any) error
 		Update(uuid.UUID, string, any) error
+		UpdateMany(uuid.UUID, any) error
 		Get(uuid.UUID) (*entity.Form, error)
 		DeleteForm(uuid.UUID) error
 		DeleteQuestion(uuid.UUID, uint) error
@@ -21,7 +22,7 @@ type (
 	}
 
 	Casher interface {
-		DoCashing(ctx context.Context, key string, payload any) error // payload must to be pointer
+		DoCashing(ctx context.Context, key string, payload any) error // payload must be pointer
 		GetCashFor(ctx context.Context, key string) ([]byte, error)
 		RemoveFromCash(ctx context.Context, key string) error
 	}
