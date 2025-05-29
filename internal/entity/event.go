88729b5b2e24601294cc4869cc70e91970b6cee0
Osamikoyo <v.pylayev@mail.ru>
2025-05-29 07:26:14 +0500

    update internal/entity/event.go

--- internal/entity/event.go
+++ internal/entity/event.go
@@ -1,6 +1,7 @@
 package entity
 
 import (
+	"errors"
 	"time"
 
 	"github.com/google/uuid"
@@ -20,4 +21,20 @@ func NewEvent(Type string, payload []byte) *Event {
 		Type:      Type,
 		Timestamp: time.Now(),
 	}
+}
+
+func (e *Event) Validate() error {
+	if e.ID == "" {
+		return errors.New("event_id is nil")
+	}
+
+	if e.Payload == nil {
+		return errors.New("payload is nil")
+	}
+
+	if e.Type == "" {
+		return errors.New("type is nil")
+	}
+
+	return nil
 }
