2107187214481f5d3c9ad2e3f464bb3053cc8474
Osamikoyo <v.pylayev@mail.ru>
2025-06-01 08:37:46 +0500

    update pkg/closer/closer.go

--- pkg/closer/closer.go
+++ pkg/closer/closer.go
@@ -1,28 +1,35 @@
 package closer
 
+import (
+	"github.com/Koyo-os/form-service/pkg/logger"
+	"go.uber.org/zap"
+)
+
 type (
-	Closer interface{
+	Closer interface {
 		Close() error
 	}
 
 	CloserGroup struct {
 		closers []Closer
+		logger  *logger.Logger
 	}
 )
 
-func NewCloserGroup(closers ...Closer) *CloserGroup {
+func NewCloserGroup(logger *logger.Logger, closers ...Closer) *CloserGroup {
 	return &CloserGroup{
 		closers: closers,
+		logger:  logger,
 	}
 }
 
 func (c *CloserGroup) Close() error {
 	var err error
-	
+
 	for _, closer := range c.closers {
 		if err := closer.Close(); err != nil {
-			return err
+			c.logger.Error("failed close", zap.Error(err))
 		}
 	}
-	return err	
+	return err
 }
